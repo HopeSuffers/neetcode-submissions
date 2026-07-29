f1f920c4a2b563d11b5a807872916e086f069d8a
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-07-29 09:55:15 +0200

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public List<List<int>> Subsets(int[] nums)
+    {
+        var returnList = new List<List<int>>();
+        var subset = new List<int>();
+
+        void Dfs(int index) {
+            if (index >= nums.Length) {
+                returnList.Add(new List<int>(subset));
+                return;
+            }
+
+            subset.Add(nums[index]);
+            Dfs(index + 1);
+
+            subset.RemoveAt(subset.Count - 1);
+            Dfs(index + 1);
+        }
+
+        Dfs(0);
+        return returnList;
+    }
+}
