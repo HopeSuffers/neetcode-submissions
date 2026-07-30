039d7bef8352c0ceb04141cfe7f64a55c841c119
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-07-30 09:06:22 +0200

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<int>> SubsetsWithDup(int[] nums) {
+        Array.Sort(nums);
+        var returnList = new List<List<int>>();
+        var current = new List<int>();
+
+        void Dfs(int index) {
+            if (index >= nums.Length) {
+                returnList.Add(new List<int>(current));
+                return;
+            }
+
+            current.Add(nums[index]);
+            Dfs(index + 1);
+
+            current.RemoveAt(current.Count - 1);
+
+            while (index + 1 < nums.Length && nums[index] == nums[index + 1]) {
+                index++;
+            }
+
+            Dfs(index + 1);
+        }
+
+        Dfs(0);
+        return returnList;
+    }
+}
