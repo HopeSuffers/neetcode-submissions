201683db9faa6b8240b9b29e805150a29a2a8d29
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-07-31 09:44:41 +0200

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<int>> CombinationSum(int[] nums, int target)
+    {
+        var listReturn = new List<List<int>>();
+        var current = new List<int>();
+
+        void Dfs(int index, int difference)
+        {
+            if (difference == 0)
+            {
+                listReturn.Add(new List<int>(current));
+                return;
+            }
+            if (index >= nums.Length || difference < 0)
+                return;
+            
+            current.Add(nums[index]);
+            Dfs(index, difference - nums[index]);
+            current.RemoveAt(current.Count - 1);
+            Dfs(index + 1, difference);
+        }
+        Dfs(0, target);
+        return listReturn;
+    }
+}
+
