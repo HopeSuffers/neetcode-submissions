4f669e66e72efa573b8e031250e7d7be55d08579
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-07-31 20:30:39 +0200

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.cs
@@ -0,0 +1,40 @@
+public class Solution {
+     public bool Exist(char[][] board, string word)
+    {
+        var current = "";
+
+        for (int row = 0; row < board.Length; row++)
+        {
+            for (int col = 0; col < board[0].Length; col++)
+            {
+                if (Dfs(row, col, 0))
+                    return true;
+            }
+        }
+        return false;
+
+        bool Dfs(int row, int col, int index)
+        {
+            if (index == word.Length)
+                return true;
+
+            if (row < 0 || row >= board.Length)
+                return false;
+
+            if (col < 0 || col >= board[0].Length)
+                return false;
+
+            if (word[index] != board[row][col])
+                return false;
+            var original = board[row][col];
+            board[row][col] = '#';
+            var result = Dfs(row + 1, col, index + 1) ||
+                         Dfs(row, col + 1, index + 1) ||
+                         Dfs(row - 1, col, index + 1) ||
+                         Dfs(row, col - 1, index + 1);
+
+            board[row][col] = original;
+            return result;
+        }
+    }
+}
