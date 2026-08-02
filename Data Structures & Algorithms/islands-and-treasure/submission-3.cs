d14f16a9404bd7cbe0c4dc6a0739ee6eb247a1db
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-02 13:34:36 +0200

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        for (int row = 0; row < grid.Length; row++) {
+            for (int col = 0; col < grid[0].Length; col++) {
+                if (grid[row][col] != int.MaxValue)
+                    continue;
+
+                int bestDistance = int.MaxValue;
+
+                Dfs(row, col, 0);
+
+                grid[row][col] = bestDistance;
+
+                void Dfs(int currentRow, int currentCol, int distance) {
+                    if (currentRow < 0 || currentRow >= grid.Length)
+                        return;
+
+                    if (currentCol < 0 || currentCol >= grid[0].Length)
+                        return;
+
+                    if (grid[currentRow][currentCol] == -1)
+                        return;
+
+                    if (distance >= bestDistance)
+                        return;
+
+                    if (grid[currentRow][currentCol] == 0) {
+                        bestDistance = Math.Min(bestDistance, distance);
+                        return;
+                    }
+
+                    var original = grid[currentRow][currentCol];
+                    grid[currentRow][currentCol] = -1;
+
+                    Dfs(currentRow + 1, currentCol, distance + 1);
+                    Dfs(currentRow - 1, currentCol, distance + 1);
+                    Dfs(currentRow, currentCol + 1, distance + 1);
+                    Dfs(currentRow, currentCol - 1, distance + 1);
+
+                    grid[currentRow][currentCol] = original;
+                }
+            }
+        }
+    }
+}
