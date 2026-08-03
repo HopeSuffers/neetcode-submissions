47d601642f5d8939cc09da3544667a125bd8473d
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-03 18:18:07 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cs
@@ -0,0 +1,49 @@
+public class Solution {
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        var dic = new Dictionary<int, List<int>>();
+
+        for (int i = 0; i < numCourses; i++)
+            dic[i] = new List<int>();
+
+        foreach (var prerequisite in prerequisites)
+        {
+            var course = prerequisite[0];
+            var requisite = prerequisite[1];
+
+            dic[course].Add(requisite);
+        }
+
+        var hashSet = new HashSet<int>();
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (Dfs(i))
+                return false;
+        }
+
+        return true;
+
+        bool Dfs(int course)
+        {
+            if (hashSet.Contains(course))
+                return true;
+
+            if (dic[course].Count == 0)
+                return false;
+
+            hashSet.Add(course);
+
+            foreach (var requisite in dic[course])
+            {
+                if (Dfs(requisite))
+                    return true;
+            }
+
+            hashSet.Remove(course);
+            dic[course].Clear();
+
+            return false;
+        }
+    }
+}
