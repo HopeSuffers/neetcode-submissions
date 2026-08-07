d121e174fbca02a02258e1258375a94819908f55
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-07 10:02:41 +0200

    Add: kth-largest-element-in-an-array - submission-3

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-3.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        var queue = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+
+        foreach (var num in nums) queue.Enqueue(num, num);
+
+        for (int i = 1; i < k; i++) queue.Dequeue();
+
+        return queue.Peek();
+    }
+}
