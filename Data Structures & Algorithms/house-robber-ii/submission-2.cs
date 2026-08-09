1fc5623e046fc459bd79c0226198b8e4f306a00e
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-09 19:06:42 +0200

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.cs
@@ -0,0 +1,32 @@
+public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        if (nums.Length == 1)
+            return nums[0];
+
+        return Math.Max(
+            RobRange(nums, 0, nums.Length - 2),
+            RobRange(nums, 1, nums.Length - 1)
+        );
+    }
+
+    private int RobRange(int[] nums, int start, int end)
+    {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            int current = Math.Max(
+                prev1,       
+                prev2 + nums[i] 
+            );
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
