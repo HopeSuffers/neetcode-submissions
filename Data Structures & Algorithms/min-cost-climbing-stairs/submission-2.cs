c1516a14a86341fdee87f2078c320a5385d21c05
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-20 19:42:49 +0200

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int MinCostClimbingStairs(int[] cost) {
+        for (int i = cost.Length - 3; i >= 0; i--) {
+            cost[i] = Math.Min(cost[i] + cost[i + 2], cost[i + 1] + cost[i]);
+        }
+
+        return Math.Min(cost[0], cost[1]);
+    }
+}
