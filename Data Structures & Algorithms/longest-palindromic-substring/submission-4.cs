40920cc388fd3b99e129a7d747a51acc058382c6
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-22 13:24:11 +0200

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public string LongestPalindrome(string s)
+    {
+        if (s.Length < 2)
+            return s;
+
+        var bestStart = 0;
+        var bestEnd = 1;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+               Expand(i,i);
+               Expand(i, i+1);
+        }
+
+        void Expand(int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                var length = right - left + 1;
+                if (length >= bestEnd)
+                {
+                    bestStart = left;
+                    bestEnd = length;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        return s.Substring(bestStart, bestEnd);
+    }
+}
