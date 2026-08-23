5849f09e74693ee262657a8e2ff65e911d046385
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-23 11:46:32 +0200

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool CanPartition(int[] nums) {
+        int total = nums.Sum();
+
+        if (total % 2 == 1)
+            return false;
+
+        var target = total / 2;
+
+        var dp = new bool[target + 1];
+        dp[0] = true;
+
+        foreach (var num in nums) {
+            for (int sum = target; sum >= num; sum--) {
+                dp[sum] = dp[sum] || dp[sum - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
