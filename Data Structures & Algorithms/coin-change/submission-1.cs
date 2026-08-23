180be9dc02bc7ac8786b481fab4e16bdbc58b2f4
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-23 09:36:42 +0200

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int CoinChange(int[] coins, int amount) {
+        var dp = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+            foreach (var coin in coins) {
+                if (coin <= i)
+                    dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
