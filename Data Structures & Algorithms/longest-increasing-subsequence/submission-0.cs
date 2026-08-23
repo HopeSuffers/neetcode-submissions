3d7359cfe54f7db0fe6abd16157e100a79ad1c36
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-23 10:47:50 +0200

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        var dp = new int[nums.Length + 1];
+
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            dp[i] = 1;
+
+            for (int j = i; j < nums.Length; j++) {
+                if (nums[j] > nums[i])
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+            }
+        }
+
+        return dp.Max();
+    }
+}
