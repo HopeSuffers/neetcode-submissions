2101c41cbe2da6059f5f801cb98ef3eb5bb49e72
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-24 09:45:42 +0200

    Add: house-robber-ii - submission-8

---
+++ Data Structures & Algorithms/house-robber-ii/submission-8.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        if (nums.Length < 2)
+            return nums.Max();
+
+        return Math.Max(Robber(0, nums.Length - 1), Robber(1, nums.Length));
+
+        int Robber(int start, int end)
+        {
+            var next = 0;
+            var nextNext = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                var current = Math.Max(nextNext + nums[i], next);
+                nextNext = next;
+                next = current;
+            }
+
+            return next;
+        }
+    }
+}
