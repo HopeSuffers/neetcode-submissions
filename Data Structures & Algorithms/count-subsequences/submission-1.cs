c2d2ff860b9c293166f5adaa06a3415ee4dc90f7
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-27 18:28:45 +0200

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int NumDistinct(string s, string t)
+    {
+        var memo = new Dictionary<(int, int), int>();
+        return Dfs(0, 0);
+
+        int Dfs(int i, int j)
+        {
+            if (j == t.Length)
+                return 1;
+
+            if (i == s.Length)
+                return 0;
+
+            if (memo.ContainsKey((i, j)))
+                return memo[(i, j)];
+
+            int result = 0;
+
+            result += Dfs(i + 1, j);
+            if (s[i] == t[j])
+            {
+               result+= Dfs(i + 1, j + 1);
+            }
+
+            memo[(i, j)] = result;
+            return result;
+
+        }
+    }
+}
