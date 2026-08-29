ab4de792cfed490e734ec57685126479ffd10b0c
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-08-29 10:58:15 +0200

    Add: burst-balloons - submission-2

---
+++ Data Structures & Algorithms/burst-balloons/submission-2.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int MaxCoins(int[] nums)
+    {
+        var list = nums.ToList();
+        list.Insert(0, 1);
+        list.Add(1);
+
+        var memo = new Dictionary<(int, int), int>();
+        return Dfs(1, list.Count - 2);
+
+        int Dfs(int left, int right)
+        {
+            if (left > right)
+                return 0;
+
+            if (memo.ContainsKey((left, right)))
+                return memo[(left, right)];
+
+            var best = 0;
+            for (int i = left; i < right+1; i++)
+            {
+                var coins = list[left - 1] * list[i] * list[right + 1];
+                coins += Dfs(left, i - 1) + Dfs(i + 1, right);
+                best = Math.Max(best, coins);
+            }
+
+            memo[(left, right)] = best;
+            return best;
+        }
+    }
+}
