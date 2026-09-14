8f23493e62b28085b68fd545e326ca240d83a3b5
Maurice Ach <80710247+HopeSuffers@users.noreply.github.com>
2026-09-14 19:46:15 +0200

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cs
@@ -0,0 +1,16 @@
+public class Solution 
+{
+    public bool hasDuplicate(int[] nums)
+    {
+        var hashSet = new HashSet<int> ();
+
+        foreach (int num in nums){
+            if (hashSet.Contains(num))
+                return true;
+
+            hashSet.Add(num);
+        }
+
+        return false;
+    }
+}
